55ee403a037d0c07792b6c43991943a53f82c088
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-12-05 00:04:06 +0000

    migrating squarkus -> spring boot

---
+++ smart-queue-api/src/main/java/com/queue/api/smartqueueapi/SmartQueueApiApplication.java
@@ -0,0 +1,13 @@
+package com.queue.api.smartqueueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQueueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartQueueApiApplication.class, args);
+	}
+
+}
---
+++ smart-queue-api/src/test/java/com/queue/api/smartqueueapi/SmartQueueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.api.smartqueueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
